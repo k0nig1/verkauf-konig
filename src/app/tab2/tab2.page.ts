d7eb6ea1ddcad81c3aa717c5e4d589955ef8991c
Dan <daniel.holterhaus@gmail.com>
2023-05-15 19:05:19 -0400

    working this tutorial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
-import { PhotoService, UserPhoto } from '../services/photo.service';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-tab2',
@@ -9,38 +9,146 @@ import { PhotoService, UserPhoto } from '../services/photo.service';
 })
 
 export class Tab2Page {
+  @ViewChild('video', { static: false }) video!: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas!: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput!: ElementRef;
 
-  constructor(public photoService: PhotoService,
-    public actionSheetController: ActionSheetController) { }
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null;
 
-  async ngOnInit() {
-    await this.photoService.loadSaved();
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
   }
 
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
   }
 
-  public async showActionSheet(photo: UserPhoto, position: number) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Photos',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          this.photoService.deletePicture(photo, position);
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
         }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          // Nothing to do, action sheet is automatically closed
-        }
-      }]
+      ]
     });
-    await actionSheet.present();
+    toast.present();
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+
+  handleFile(files: FileList) {
+    const file = files.item(0);
+
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
   }
 
 }
