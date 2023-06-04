f323166b4fa8a8df7e238c11062ba0147b93f5d6
Daniel Holterhaus <daniel.holterhaus@gmail.com>
2023-06-04 19:07:36 -0400

    more refactoring

--- src/app/order/order.page.ts
+++ src/app/order/order.page.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { QrScannerService } from '../../services/qr-scanner.service';
+import { QrScannerService } from '../services/qr-scanner.service';
 
 @Component({
   selector: 'app-order',
