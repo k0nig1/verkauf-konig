47d4bef1ce73c79a78694903cc9c203845428b49
dan <dan@UbuntuVk.myguest.virtualbox.org>
2023-06-09 12:11:17 -0400

    init user profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'user-profile',
+    loadChildren: () => import('./pages/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
   }
 ];
 @NgModule({
